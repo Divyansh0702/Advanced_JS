53035a0226bce103e8221ec5a39352d530b4c2a9
Divyansh0702 <divyansh07020@gmail.com>
2025-04-28 16:22:22 +0530

    ArraysInclude

---
+++ ArraysInclude.js
@@ -0,0 +1,14 @@
+// const fruits = ["banana", "orange", "apples"];
+// console.log(fruits.includes("Mango"));
+
+// Trailing Comma
+
+// const person = {
+//     firstname : "John",
+//     lastname : "Doe",
+//     age : 50
+// };
+
+const person = {
+    firstname : "John",
+}
